cc82e96a007075c582e7b4483a7c08bc42d64d7e
Saurabh Zade <47217606+saurabh077@users.noreply.github.com>
2021-01-27 19:37:51 +0530

    Update Numbers.c
    Done changes acc to reviews

--- Numbers.c
+++ Numbers.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
-#define MAX_STRING_SIZE 15
+#define MAX_STRING_LEN 14
 int main(){
-	char numbers[][MAX_STRING_SIZE] = {"one","two", "three","four","five","six", "seven","eight","nine", "ten"};
+	char numbers[][MAX_STRING_LEN+1] = {"one","two", "three","four","five","six", "seven","eight","nine", "ten"};
 	int i=0;
 	for(i=0;i<10;i++)
+	{
 		printf("\n%d : %s", i+1,numbers[i]);
-		
+	}	
 	return 0;
 }
