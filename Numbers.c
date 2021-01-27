34b35031cb13a772f3fa64e80dd4fbe40929078a
Saurabh Zade <47217606+saurabh077@users.noreply.github.com>
2021-01-27 19:44:42 +0530

    Update Numbers.c
    \n

--- Numbers.c
+++ Numbers.c
@@ -5,7 +5,7 @@ int main()
 	char numbers[][MAX_STRING_LEN+1] = {"one","two", "three","four","five","six", "seven","eight","nine", "ten"};
 	int i=0;
 	for(i=0;i<10;i++){
-		printf("\n%d : %s", i+1,numbers[i]);
+		printf("%d : %s\n", i+1,numbers[i]);
 	}	
 	return 0;
 }
