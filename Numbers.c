1e86b1b12d8e4d816d0a960574870120cb8898d6
Saurabh Zade <47217606+saurabh077@users.noreply.github.com>
2021-01-27 19:41:54 +0530

    Update Numbers.c
    Changes for the curely braces

--- Numbers.c
+++ Numbers.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #define MAX_STRING_LEN 14
-int main(){
+int main()
+{
 	char numbers[][MAX_STRING_LEN+1] = {"one","two", "three","four","five","six", "seven","eight","nine", "ten"};
 	int i=0;
-	for(i=0;i<10;i++)
-	{
+	for(i=0;i<10;i++){
 		printf("\n%d : %s", i+1,numbers[i]);
 	}	
 	return 0;
